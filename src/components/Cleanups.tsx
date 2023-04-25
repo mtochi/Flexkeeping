0181562cf8942800d4ac29b25c735be6c5d9a84e
mtochi <medinatoci@hotmail.com>
2023-04-25 17:55:09 +0200

    ui changes

--- src/components/Cleanups.tsx
+++ src/components/Cleanups.tsx
@@ -1,7 +1,8 @@
 import Box from '@mui/material/Box';
+import { useMediaQuery } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
 import { useDatesDispatch } from '../contexts/DateContext';
 import { useDates } from '../contexts/DateContext';
 import { BarChart } from './BarChart';
@@ -13,51 +14,80 @@ export const Cleanups = () => {
   const dispatch = useDatesDispatch();
   const dates = useDates();
   console.log(dates)
+  const isMobile = useMediaQuery('(max-width:600px)');
 
   return (
     <Box sx={{ width: '100%' }}>
-    <Grid container rowSpacing={3} columnSpacing={0} sx={{marginTop: 3}}>
-      <Grid item xs={6}>
-        <DatePicker
-          minDate={dayjs('2023-04-01')}
-          maxDate={dayjs('2023-04-07')}
-          onChange={(e) => {
-            dispatch({
-              type: 'SET_START_DATE',
-              date: e,
-            });
-          }} />
-      </Grid>
-      <Grid item xs={6}>
+      <Grid container rowSpacing={3} columnSpacing={0} sx={{ marginTop: 3 }}>
+        <Grid item md={6} xs={12}>
+          <DatePicker
+            minDate={dayjs('2023-04-01')}
+            maxDate={dayjs('2023-04-07')}
+            onChange={(e) => {
+              dispatch({
+                type: 'SET_START_DATE',
+                date: e,
+              });
+            }} />
+        </Grid>
+        <Grid item md={6} xs={12}>
 
-        <DatePicker
-          minDate={dayjs('2023-04-01')}
-          maxDate={dayjs('2023-04-07')}
-          onChange={(e) => {
-            dispatch({
-              type: 'SET_END_DATE',
-              date: e,
-            });
-          }} />
-      </Grid>
+          <DatePicker
+            minDate={dayjs('2023-04-01')}
+            maxDate={dayjs('2023-04-07')}
+            onChange={(e) => {
+              dispatch({
+                type: 'SET_END_DATE',
+                date: e,
+              });
+            }} />
+        </Grid>
 
-      <Grid item xs={2}>
-      <Box sx={{ height: '250px', overflowY: 'scroll' }}>
+        {
+          isMobile ? (
+
+
+            <Grid container rowSpacing={3} columnSpacing={0}>
+               <Grid item xs={12} md={2}>
+          <Box sx={{ height: '250px', overflowY: 'scroll' }}>
             <HouseKeepers />
           </Box>
-      </Grid>
-      <Grid item xs={4}>
-        <DoughnutChart />
-      </Grid>
-      <Grid item xs={4}>
-        <BarChart />
-      </Grid>
-      <Grid item xs={2}>
-      <Box sx={{ height: '200px', overflowY: 'scroll' }}>
-            <Spaces />
+        </Grid>
+        <Grid item xs={12} md={2}>
+                <Box sx={{ height: '200px', overflowY: 'scroll' }}>
+                  <Spaces />
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <DoughnutChart />
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <BarChart />
+              </Grid>
+             
+            </Grid>
+          ) : (
+            <Grid container rowSpacing={3} columnSpacing={0}>
+                      <Grid item xs={12} md={2}>
+          <Box sx={{ height: '250px', overflowY: 'scroll' }}>
+            <HouseKeepers />
           </Box>
+        </Grid>
+            
+            <Grid item xs={12} md={4}>
+              <DoughnutChart />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <BarChart />
+            </Grid>
+            <Grid item xs={12} md={2}>
+              <Box sx={{ height: '200px', overflowY: 'scroll' }}>
+                <Spaces />
+              </Box>
+            </Grid>
+            </Grid>)
+        }
       </Grid>
-    </Grid>
     </Box>
   );
 }
